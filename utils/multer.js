e3888004e64b318d412618906f1f311a836ff6d0
yayanfr20 <yayanfathurohman20@gmail.com>
2023-11-03 14:45:47 +0700

    update destination upload

--- utils/multer.js
+++ utils/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 const uploader = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "uploads/");
+      cb(null, "public/uploads/");
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname);
